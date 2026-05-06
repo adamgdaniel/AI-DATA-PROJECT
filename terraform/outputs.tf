8c00618e020b9de11b8b2c5fddb6d98021cf263d
florsastre-gif <florsastre77@gmail.com>
2026-05-06 21:37:47 +0200

    fix: añadir outputs de URLs de los servicios de IA en terraform

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -9,3 +9,19 @@ output "api_url" {
 output "db_connection_name" {
   value = google_sql_database_instance.main.connection_name
 }
+
+output "agent_url" {
+  value = google_cloud_run_v2_service.agent.uri
+}
+
+output "rag_api_url" {
+  value = google_cloud_run_v2_service.rag_api.uri
+}
+
+output "sensor_api_url" {
+  value = google_cloud_run_v2_service.sensor_api.uri
+}
+
+output "model_serving_url" {
+  value = google_cloud_run_v2_service.model_serving.uri
+}
